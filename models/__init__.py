fb2ca67fcd13df1be55027c0e5543d128a1f89d5
ziontsion <tsionnew@gmail.com>
2023-06-20 23:18:36 -0700

    database storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Instantiates a storage object.
+"""This module instantiates an object of class FileStorage"""
+import os
 
--> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
-   instantiates a database storage engine (DBStorage).
--> Otherwise, instantiates a file storage engine (FileStorage).
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+
+storage = DBStorage() if os.getenv(
+    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
+"""A unique FileStorage/DBStorage instance for all models.
 """
-from os import getenv
-
-
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
 storage.reload()
